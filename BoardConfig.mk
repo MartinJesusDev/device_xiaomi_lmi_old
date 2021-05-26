567947c5d40f3dd98b2d27c90ac360d4e74c2a27
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-05-26 21:19:17 +0200

    lmi: Update TARGET_KERNEL_CONFIG

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,9 @@ SOONG_CONFIG_XIAOMI_KONA_FOD_POS_X = 439
 SOONG_CONFIG_XIAOMI_KONA_FOD_POS_Y = 1655
 SOONG_CONFIG_XIAOMI_KONA_FOD_SIZE = 202
 
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/lmi_defconfig
+
 # Inherit from xiaomi sm8250-common
 include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
